5d8e875fd29c4616870881c6860c49ec99876791
w03 <1341362264@qq.com>
2022-08-25 15:18:41 +0800

    123

--- lib/index.ts
+++ lib/index.ts
@@ -1,31 +1,31 @@
 
 // send()
-// import schedule from 'node-schedule';
-// console.log('[ schedule ]', schedule)
+import schedule from 'node-schedule';
+console.log('[ schedule ]', schedule)
 
-// const job = schedule.scheduleJob('52 14 * * *', function () {
-//   console.log('The answer to life, the universe, and everything!');
-// });
-import { SimpleIntervalJob, Task, ToadScheduler } from 'toad-scheduler';
-let i = 1
-const scheduler = new ToadScheduler();
-const task = new Task('simple task', () => {
-  console.log('Task triggered');
-  i++
-  if (i >= 100) {
-    scheduler.stopById('id_1')
-  }
+const job = schedule.scheduleJob('52 14 * * *', function () {
+  console.log('The answer to life, the universe, and everything!');
 });
+// import { SimpleIntervalJob, Task, ToadScheduler } from 'toad-scheduler';
+// let i = 1
+// const scheduler = new ToadScheduler();
+// const task = new Task('simple task', () => {
+//   console.log('Task triggered');
+//   i++
+//   if (i >= 100) {
+//     scheduler.stopById('id_1')
+//   }
+// });
 
-const job1 = new SimpleIntervalJob(
-  { seconds: 1, runImmediately: true },
-  task,
-  'id_1'
-);
+// const job1 = new SimpleIntervalJob(
+//   { seconds: 1, runImmediately: true },
+//   task,
+//   'id_1'
+// );
 
 
-//create and start jobs
-scheduler.addSimpleIntervalJob(job1);
+// //create and start jobs
+// scheduler.addSimpleIntervalJob(job1);
 
 // stop job with ID: id_2
 // scheduler.stopById('id_2');
